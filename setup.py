6883304ecb974a9bc6f44249c1b26aa13fea112a
Lawrence Neal <nealla@lwneal.com>
2016-12-22 18:16:54 -0800

    Add setup.py to install 'word'

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='word',
+    version='1.0',
+    description='Python replacement for awk',
+    author='lwneal',
+    author_email='nealla@lwneal.com',
+    url='https://github.com/lwneal/word',
+    scripts=['scripts/word'],
+)
